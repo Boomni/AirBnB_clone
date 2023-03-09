2722922b434037931863ba3e000c1177f5cb6e71
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-03-09 07:31:21 +0100

    checked all python files if pycodestyle compliant

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,7 +25,7 @@ class FileStorage:
         """
         Sets in __objects the obj with key <obj class name>.id
         """
-        key = obj.__class__.__name__ + "." + obj.id
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
