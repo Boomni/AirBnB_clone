7b355c80391ab7d3fb9fd884c9591331b4e03f73
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-03-09 20:22:36 +0100

    implimented create, show, destroy, all, update commands on the command interpreter file

--- console.py
+++ console.py
@@ -4,6 +4,8 @@ Module that contains the entry point of the command interpreter
 """
 import cmd
 from models.base_model import BaseModel
+from models import storage
+
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -27,8 +29,10 @@ class HBNBCommand(cmd.Cmd):
         """Do nothing on empty line
         """
         pass
+
     def do_create(self, arg):
-        """Creates a new instance of BaseModel, saves it to JSON file,
+        """
+        Creates a new instance of BaseModel, saves it to JSON file,
         and prints the id
         Eg: $ create BaseModel
         """
@@ -40,6 +44,99 @@ class HBNBCommand(cmd.Cmd):
             the_class = eval(arg)()
             the_class.save()
             print(the_class.id)
+
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance
+        based on the class name and id.
+        Eg: $ show BaseModel 1234-1234-1234
+        """
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.class_instructions.keys():
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            obj_id = args[0] + "." + args[1]
+            obj_dict = storage.all()
+            if obj_id in obj_dict:
+                print(obj_dict[obj_id])
+            else:
+                print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id
+        (save the change into the JSON file).
+        Eg: $ destroy BaseModel 1234-1234-1234
+        """
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.class_instructions.keys():
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            obj_id = args[0] + "." + args[1]
+            obj_dict = storage.all()
+            if obj_id in obj_dict:
+                del obj_dict[obj_id]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        """
+        Prints all string representation of all instances
+        based on or not on the class name.
+        Ex: $ all BaseModel or $ all.
+        """
+        result = []
+        obj_dict = storage.all()
+        for key, value in obj_dict.items():
+            if not arg:
+                result.append(str(value))
+            else:
+                if value.__class__.__name__ == arg:
+                    result.append(str(value))
+        print(result)
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id
+        by adding or updating attribute (save the change into the JSON file)
+        Ex: $ update BaseModel 1234-1234-1234 email "aibnb@mail.com"
+        """
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.class_instructions.keys():
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            obj_id = args[0] + "." + args[1]
+            obj_attribute_key = args[0] + "." + args[2]
+            obj_attribute_value = args[3]
+            not_accepted = ["id", "created_at", "updated_at"]
+            obj_dict = storage.all()
+            if obj_id not in obj_dict:
+                print("** no instances found **")
+            elif len(args) < 3:
+                print("** attribute name missing **")
+            elif len(args) < 4:
+                print("** value missing **")
+            elif len(args) > 4:
+                pass
+            else:
+                if args[2] in not_accepted:
+                    pass
+                else:
+                    obj_dict[obj_id].__dict__[obj_attribute_key] = obj_attribute_value
+                    obj_dict[obj_id].save()
 
 
 if __name__ == '__main__':
