8e0765f4c77348d8fbca869a6e693b1ce9e133b1
Jonathan Boomni <110749205+Boomni@users.noreply.github.com>
2023-03-10 23:51:14 +0100

    Added all stributes

--- console.py
+++ console.py
@@ -26,6 +26,40 @@ class HBNBCommand(cmd.Cmd):
                           "Amenity": Amenity,
                           "Place": Place,
                           "Review": Review}
+    class_attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+                     {"place_id": str,
+                      "user_id": str,
+                      "text": str}
+        }
 
     def do_quit(self, arg):
         """Quit command to exit the program
